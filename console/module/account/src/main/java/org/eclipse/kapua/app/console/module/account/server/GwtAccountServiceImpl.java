323a32b4db648b3fa7539e85e8d8f11a73103fbb
Bojan Utkovic <bojan.utkovic@comtrade.com>
2018-01-26 14:21:06 +0100

    non sprecific error fixes
    Signed-off-by: Bojan Utkovic <bojan.utkovic@comtrade.com>

--- console/module/account/src/main/java/org/eclipse/kapua/app/console/module/account/server/GwtAccountServiceImpl.java
+++ console/module/account/src/main/java/org/eclipse/kapua/app/console/module/account/server/GwtAccountServiceImpl.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2017 Eurotech and/or its affiliates and others
+ * Copyright (c) 2017, 2018 Eurotech and/or its affiliates and others
  *
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
@@ -127,13 +127,6 @@ public class GwtAccountServiceImpl extends KapuaRemoteServiceServlet implements 
 
             // create the Account
             AccountService accountService = locator.getService(AccountService.class);
-            AccountQuery query = accountFactory.newQuery(parentAccountId);
-            AccountListResult list = accountService.query(query);
-            for (Account account : list.getItems()) {
-                if (account.getName().equals(gwtAccountCreator.getAccountName())) {
-                    throw new KapuaDuplicateNameException(gwtAccountCreator.getAccountName());
-                }
-            }
             Account account = accountService.create(accountCreator);
 
             // convertKapuaId to GwtAccount and return
